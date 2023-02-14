f5f4e89df8c2ad42ff037f9a87c68a1c2ba73e29
yanenko <without1481@gmail.com>
2023-02-14 11:29:20 +0200

    initial commit

---
+++ Notes/Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Petr Yanenko on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
